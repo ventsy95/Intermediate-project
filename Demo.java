2237121911d50f45759a5d6fe1c56262fe5ce0ce
ventsy95 <ventsy95@mail.bg>
2016-08-18 13:06:09 +0300

    Demo class started

---
+++ Demo.java
@@ -0,0 +1,22 @@
+import java.util.Map.Entry;
+
+public class Demo {
+	public static void main(String[] args) {
+		User ivo = new RegisteredUser("Ivo", "ivo5", "089856587", "Sofia", "ivo@gmail.com");
+		//System.out.println(ivo.getPassword());
+		/*System.out.println("Tova e parolata sega : " + ivo.getPassword());
+		ivo.changePassword();
+		System.out.println(ivo.getPassword());
+		*/
+		
+		User iva = new RegisteredUser("Iva", "iva5", "089856587", "Sofia", "ivo@gmail.com");
+		
+		ivo.sendMessage(iva, new Message("Imam interes", "Iskam da napravq ogled"));
+		ivo.sendMessage(iva, new Message("Nqmam interes", "Basi skupotiqta"));
+		
+		iva.getMessage();
+		
+		ivo.getMessage();
+	}
+
+}
