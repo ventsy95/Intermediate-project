af4c10dce110d31aa7d7468baaa68d66e5cd45f6
ventsy95 <ventsy95@mail.bg>
2016-08-18 12:30:08 +0300

    Registered user class started with unimplemented methods

--- RegisteredUser.java
+++ RegisteredUser.java
@@ -1,4 +1,55 @@
 
-public class RegisteredUser {
+public class RegisteredUser extends User {
+	private boolean isVip;
 
+	public RegisteredUser(String name, String userName, String password, String phoneNumber, String address,
+			String email) {
+		super(name, userName, password, phoneNumber, address, email);
+	}
+	
+	public void requestVipUpgrade(){
+		
+	}
+
+	@Override
+	public void uploadAd(Ad ad) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeVipUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void upgrade(Ad ad) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changePassword() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendMessage(User user, Message message) {
+		// TODO Auto-generated method stub
+		
+	}
 }
