80dbf81e64a25a45c0d774d5fd535894ed5f7448
ventsy95 <ventsy95@mail.bg>
2016-08-18 12:50:35 +0300

    Agency class created.

---
+++ Agency.java
@@ -0,0 +1,12 @@
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Agency {
+	String name;
+	// brokers username and ad
+	Map<String, List<Ad>> obqviNaAgenciqta;
+	Set<Broker> brokers;
+
+}
