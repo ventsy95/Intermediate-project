dc6d9eaa4537130d55e7ae74b79857ded6f4a699
ventsy95 <ventsy95@mail.bg>
2016-08-18 12:35:07 +0300

    Exception for missmatch in user imput created.

---
+++ ImputMismatchException.java
@@ -0,0 +1,23 @@
+
+public class ImputMismatchException extends Exception{
+
+	private static final long serialVersionUID = 4700754780077805979L;
+
+	public ImputMismatchException() {
+		super();
+	}
+
+	public ImputMismatchException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ImputMismatchException(String message) {
+		super(message);
+	}
+
+	public ImputMismatchException(Throwable cause) {
+		super(cause);
+	}
+	
+
+}
